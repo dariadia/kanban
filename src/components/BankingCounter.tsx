1f07ff3730a9ccb28aa94781bf7b770d4e333208
Daria <daria.diachkova@mail.ru>
2023-08-17 11:27:41 +0200

    feat(banking): change how influx in calculated

--- src/components/BankingCounter.tsx
+++ src/components/BankingCounter.tsx
@@ -4,11 +4,10 @@ import { COMPLETE, FAILED, PENDING } from "./constants"
 export const BankingCounter: React.FC<CounterData> = ({ 
   totalTransactions, 
 }) => {
-  const influx = totalTransactions.reduce(
-    (accumulator, currentValue) => accumulator + currentValue.amount,
-    0,
-  )
   const complete = totalTransactions.filter(
+    item => item.columnUuid === COMPLETE,
+  )
+  const completeNum = totalTransactions.filter(
     item => item.columnUuid === COMPLETE,
   ).length
   const pending = totalTransactions.filter(
@@ -17,15 +16,19 @@ export const BankingCounter: React.FC<CounterData> = ({
   const failed = totalTransactions.filter(
     item => item.columnUuid === FAILED,
   ).length
+  const influx = complete.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.amount,
+    0,
+  )
   return (
     <div className="grid-2 bg-mainBackgroundColor p-2.5 items-center text-left rounded-xl border-2 sticky">
       <div>
         <b>Total transactions:</b> {totalTransactions.length}<br />
-        <b>Total influx:</b> {influx}
+        <b>Total influx:</b> {influx.toLocaleString()}
       </div>
       <div className="grid">
         <span><b>{COMPLETE}</b></span><span><b>{PENDING}</b></span><span><b>{FAILED}</b></span>
-        <span>{complete}</span><span>{pending}</span><span>{failed}</span>
+        <span>{completeNum}</span><span>{pending}</span><span>{failed}</span>
       </div>
     </div>
   )
